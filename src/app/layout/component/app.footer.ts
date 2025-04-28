29435a06e72f80a4b0d5a2074a60840de0f45ce2
tuan.nd <tuannd2207@gmail.com>
2025-04-28 14:08:51 +0700

    change theme

--- src/app/layout/component/app.footer.ts
+++ src/app/layout/component/app.footer.ts
@@ -3,9 +3,9 @@ import { Component } from '@angular/core';
 @Component({
     standalone: true,
     selector: 'app-footer',
-    template: `<div class="layout-footer">
-        SAKAI by
-        <a href="https://primeng.org" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">PrimeNG</a>
+    template: ` <div class="layout-footer">
+        Fiy-vd by
+        <a href="https://google.com" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">Google</a>
     </div>`
 })
 export class AppFooter {}
--- src/app/layout/component/app.topbar.ts
+++ src/app/layout/component/app.topbar.ts
@@ -33,7 +33,7 @@ import { LayoutService } from '../service/layout.service';
                         />
                     </g>
                 </svg>
-                <span>SAKAI</span>
+                <span>Fiy-vd</span>
             </a>
         </div>
 
